8f5bc89acf51e79a6bdb141dc1bfd632f5e3fe43
questcoderull <questcoderull@gmail.com>
2025-07-15 11:26:16 +0600

    set up firebase config and authenctication systems

---
+++ src/Context/AuthProver.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+import { auth } from "../firebase/firebase.init";
+
+const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const provider = new GoogleAuthProvider();
+
+  //   creating new user
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const logInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //   logOut user
+  const logOutUser = () => {
+    return signOut(auth);
+  };
+
+  //   sighUp/signIn with google.
+  const continueWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+
+  //   ovserber
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      // console.log("user in the auth state change", currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    loading,
+    user,
+    setLoading,
+    createUser,
+    logInUser,
+    logOutUser,
+    continueWithGoogle,
+  };
+
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
+};
+
+export default AuthProvider;

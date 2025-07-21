bf29606fadb5c5c2ca61492aa138e089595645ed
questcoderull <questcoderull@gmail.com>
2025-07-21 18:31:47 +0600

    payment form added

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,13 +1,16 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
+import PaymentForm from "./PaymentForm";
 
 const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
 
 const Payment = () => {
   return (
-    <div>
-      <Elements stripe={stripePromise}></Elements>
+    <div className="my-16">
+      <Elements stripe={stripePromise}>
+        <PaymentForm></PaymentForm>
+      </Elements>
     </div>
   );
 };
---
+++ src/Pages/Payment/PaymentForm.jsx
@@ -0,0 +1,55 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
+
+const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (!card) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      setError("");
+      console.log("payment method", paymentMethod);
+    }
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="space-y-4 bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto"
+      >
+        <CardElement className="p-2 border rounded"></CardElement>
+        <button
+          className="btn btn-primary w-full"
+          type="submit"
+          disabled={!stripe}
+        >
+          Pay to book the slot
+        </button>
+
+        {error && <p className="text-sm text-error">{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default PaymentForm;
